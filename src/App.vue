e8196e2a46570228946f03bf9115179b934984b0
mariehmai <mai.mariehelene@gmail.com>
2019-05-07 11:49:21 -0500

    feat(Landing): create landing page and clean up boilerplate files

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="HelpMe logo" src="./assets/logo.svg">
+    <Landing v-bind:menu="menu" msg="HelpMe"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Landing from "./components/Landing.vue";
+
+let menu = [
+  {
+    id: 0,
+    title: "Emergency numbers",
+    logo: "911.svg"
+  },
+  {
+    id: 1,
+    title: "Emergency contacts",
+    logo: "phone-book.svg"
+  },
+  {
+    id: 2,
+    title: "Danger alert",
+    logo: "alert.svg"
+  },
+  {
+    id: 3,
+    title: "Police stations",
+    logo: "police.svg"
+  },
+  {
+    id: 4,
+    title: "Hospitals",
+    logo: "hospital.svg"
+  }
+];
 
 export default {
-  name: 'app',
+  name: "app",
+  data: function() {
+    return {
+      menu
+    };
+  },
   components: {
-    HelloWorld
+    Landing
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Card.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="card">
+    <img class="img" v-bind:alt="logo" v-bind:src="require('../assets/menu/' + logo)">
+    <p>{{ title }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    title: String,
+    logo: String
+  }
+};
+</script>
+
+<style scoped>
+.card {
+  cursor: pointer;
+  border-radius: 8px;
+  border: 1px solid #cccccc;
+}
+
+.card:hover {
+  background-color: #cccccc;
+}
+
+.img {
+  max-width: 50px;
+}
+</style>
