854a577e470427e017a40e9c963f851dbbbf9c5c
mariehmai <mai.mariehelene@gmail.com>
2019-05-08 10:46:55 -0500

    refactor(menu): extract menu data

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -12,46 +12,14 @@
 
 <script>
 import Card from "@/components/Card.vue";
-
-const menu = [
-  {
-    id: 0,
-    titleIntlId: "menu.numbers",
-    logo: "911.svg",
-    path: "/emergency-numbers"
-  },
-  {
-    id: 1,
-    titleIntlId: "menu.contacts",
-    logo: "phone-book.svg",
-    path: "/emergency-contacts"
-  },
-  {
-    id: 2,
-    titleIntlId: "menu.alert",
-    logo: "alert.svg",
-    path: "/alert"
-  },
-  {
-    id: 3,
-    titleIntlId: "menu.police",
-    logo: "police.svg",
-    path: "/police"
-  },
-  {
-    id: 4,
-    titleIntlId: "menu.hospitals",
-    logo: "hospital.svg",
-    path: "/hospitals"
-  }
-];
+import menu from "@/data/menu";
 
 export default {
   name: "Home",
   props: {
     msg: String
   },
-  data: function() {
+  data() {
     return {
       menu
     };
---
+++ src/data/menu.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    id: 0,
+    titleIntlId: 'menu.numbers',
+    logo: '911.svg',
+    path: '/emergency-numbers'
+  },
+  {
+    id: 1,
+    titleIntlId: 'menu.contacts',
+    logo: 'phone-book.svg',
+    path: '/emergency-contacts'
+  },
+  {
+    id: 2,
+    titleIntlId: 'menu.alert',
+    logo: 'alert.svg',
+    path: '/alert'
+  },
+  {
+    id: 3,
+    titleIntlId: 'menu.police',
+    logo: 'police.svg',
+    path: '/police'
+  },
+  {
+    id: 4,
+    titleIntlId: 'menu.hospitals',
+    logo: 'hospital.svg',
+    path: '/hospitals'
+  }
+]
