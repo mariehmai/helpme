0ed1b30f8926101ac5c32bcd8e98fa03c4c33675
mariehmai <mai.mariehelene@gmail.com>
2019-05-09 11:28:56 -0500

    style(Card): update Card prop validation and hover style

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,34 +1,42 @@
 <template>
-  <div @click="onClick" id="card">
-    <img class="img" :alt="logo" :src="require('@/assets/menu/' + logo)">
-    <p>{{ title }}</p>
-  </div>
+  <v-hover>
+    <div @click="onClick" id="card" slot-scope="{ hover }" :class="`elevation-${hover ? 6 : 2}`">
+      <img class="img" :alt="logo" :src="logo">
+      <p>{{ title }}</p>
+    </div>
+  </v-hover>
 </template>
 
 <script>
 export default {
   name: "Card",
   props: {
-    title: String,
-    logo: String,
+    title: { type: String, required: true },
+    logo: { type: String, required: true },
     onClick: { type: Function, default: () => {} }
   }
 };
 </script>
 
 <style scoped>
+p {
+  margin-bottom: 0;
+}
+
 #card {
   cursor: pointer;
   border-radius: 8px;
   box-shadow: 2px 2px 4px var(--v-grey-base);
+  padding: 14px;
 }
 
 #card:hover {
   background-color: var(--v-secondary-base);
+  color: var(--v-background-base);
+  font-weight: 600;
 }
 
 .img {
   max-width: 50px;
-  padding-top: 20px;
 }
 </style>
--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,8 @@
 export default {
-  primary: '#2c3e50',
-  secondary: '#33C36C',
+  primary: {
+    base: '#2c3e50'
+  },
+  secondary: '#1FB8B3',
   accent: '#33936C',
   error: '#b71c1c',
   background: '#fafafa',
