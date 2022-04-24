233cc6e2a53162e174c0509865e592405bdfbe8c
Marie-Helene Mai <mai.mariehelene@gmail.com>
2022-04-24 21:54:18 +0100

    core(app): add tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import { i18n } from "./plugins/i18n";
 
+import "./style/global.css";
+
 const app = createApp(App);
 
 app.use(router).use(i18n);
