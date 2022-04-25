b5ba481773507eb232164f8868f9ef2a47002db7
Marie-Helene Mai <mai.mariehelene@gmail.com>
2022-04-25 10:24:44 +0100

    feat(EmergencyNumbers): add search by country

--- src/components/EmergencyNumbers.vue
+++ src/components/EmergencyNumbers.vue
@@ -3,7 +3,9 @@ import emergencyNumbers from "../data/emergencyNumbers.json";
 </script>
 
 <template>
-  <h2 class="font-semibold mt-6 lg:mt-0">{{ $t("menu.numbers") }}</h2>
+  <div class="flex flex-row justify-between gap-2 overflow-x-scroll px-1 py-2">
+    <input v-model="countrySearch" placeholder="🔍 Search... e.g. france" />
+  </div>
   <div class="border-t border border-slate-100 mt-3 mb-6" />
   <span class="grid grid-cols-4 lg:grid-cols-5 mb-2">
     <h3 class="lg:col-span-2 font-semibold">{{ $t("country") }}</h3>
@@ -11,22 +13,22 @@ import emergencyNumbers from "../data/emergencyNumbers.json";
     <h3 class="text-center font-semibold">{{ $t("service.police") }}</h3>
     <h3 class="text-center font-semibold">{{ $t("service.fire") }}</h3>
   </span>
-  <ul class="max-h-[80vh] lg:max-h-[75vh] overflow-auto gap-2 flex flex-col">
-    <li
-      class="grid grid-cols-4 lg:grid-cols-5"
-      v-for="number in numbers"
-      :key="number.Country.Name"
-    >
-      <span class="lg:col-span-2">
+  <ul class="max-h-[80vh] lg:max-h-[75vh] overflow-auto flex flex-col">
+    <li v-if="numbers.length === 0" class="text-center py-4 text-slate-400">
+      {{ $t("countryUnavailable", { country: countrySearch }) }}
+    </li>
+    <li class="grid grid-cols-4 lg:grid-cols-5 odd:bg-emerald-50 py-2" v-for="number in numbers"
+      :key="number.Country.Name">
+      <span class="lg:col-span-2 ellipsis" :title="number.Country.Name">
         {{ number.Country.ISOCode }} - {{ number.Country.Name }}
       </span>
-      <span class="text-center">
+      <span class="text-center ellipsis">
         {{ number.Ambulance.All[0] }}
       </span>
-      <span class="text-center">
+      <span class="text-center ellipsis">
         {{ number.Police.All[0] }}
       </span>
-      <span class="text-center">
+      <span class="text-center ellipsis">
         {{ number.Fire.All[0] }}
       </span>
     </li>
@@ -36,8 +38,18 @@ import emergencyNumbers from "../data/emergencyNumbers.json";
 <script lang="ts">
 export default {
   name: "emergency-numbers",
+  watch: {
+    countrySearch(newSearch: string) {
+      this.numbers = newSearch ? emergencyNumbers.data.filter(
+        number => number.Country.Name.toLowerCase()
+          .includes(newSearch.toLowerCase())) : emergencyNumbers.data
+    }
+  },
   data() {
-    return { numbers: emergencyNumbers.data };
+    return {
+      countrySearch: "",
+      numbers: emergencyNumbers.data,
+    };
   },
 };
 </script>
